40b706f74554ffe40483b6724dc9d201137d74dc
jendaaaaa <jpohludka2.0@gmail.com>
2024-04-12 01:08:33 +0200

    v1.2

--- Assets/PendulumIntegrator.cs
+++ Assets/PendulumIntegrator.cs
@@ -6,30 +6,54 @@ using UnityEngine;
 public class PendulumIntegrator : Integrator
 {
     double g_earth = 9.81;
-    public double b = 1;
-    public double m = 1;
-    public double f = 1;
-    public double gForce = 1;
     double g;
+    public double gForce;
+    public double f;
+    public double b_b;
+    public double b_c;
+    public double m_b;
+    public double m_c;
+    public double L;
+
 
     public double [] x;
-    public void setParams(double b, double m, double f, double gForce){
-        this.m = m;
-        this.b = b;
+    public void setParams(double m_b, double m_c, double b_b, double b_c, double L,  double f, double gForce){
+        this.m_b = m_b;
+        this.m_c = m_c;
+        this.b_b = b_b;
+        this.b_c = b_c;
+        this.L = L;
         this.f = f;
         this.gForce = gForce;
     }
-    public void setIC(double x0, double v0, double u0){
-        x = new double[3];
-        Init(3);
+    public void setIC(double x0, double v0, double phi0, double omega0){
+        x = new double[4];
+        Init(4);
         x[0] = x0;
         x[1] = v0;
-        x[2] = u0;
+        x[2] = phi0;
+        x[3] = omega0;
     }
     public override void RatesOfChange(double[] x, double[] xdot, double t, double u)
     {
+        // pre calculate
         g = (g_earth * gForce);
+        double sin_x2 = System.Math.Sin(x[2]);
+        double cos_x2 = System.Math.Cos(x[2]);
+        // calculate derivation
         xdot[0] = x[1];
-        xdot[1] = -(b / m) * x[1] - (g * f / m)*System.Math.Sign(x[1]) + (1 / m) * u;
+        xdot[1] = (m_b * L * System.Math.Pow(x[3], 2) * sin_x2
+                + m_b * g *  sin_x2 * cos_x2
+                - b_c * x[1]
+                + b_b / L * x[3] * cos_x2
+                + u)
+            / (m_c + m_b * System.Math.Pow(sin_x2, 2));
+        xdot[2] = x[3];
+        xdot[3] = - (m_b * L * System.Math.Pow(x[3], 2) * sin_x2 * cos_x2
+                - (m_b + m_c) * g * sin_x2
+                + b_c * x[1] * cos_x2
+                - (1 + m_c/m_b) * b_b / L * x[3]
+                - u * cos_x2)
+            / (L * (m_c + m_b * System.Math.Pow(sin_x2, 2)));
     }
 }
--- Assets/PendulumModel.cs
+++ Assets/PendulumModel.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PendulumModel : TimestepModel
 {
     public GameObject cart;
-    public GameObject ballOnBeam;
+    public GameObject beam;
     PendulumIntegrator integrator;
     double t = 0.0;
     double u = 0.0;
     // parameters to set
-    public double inputGain = 0.1;          // gain of the input from keyboard
+    public double inputGain = 1;          // gain of the input from keyboard
     public double gForce = 1;               // gain of g on earth
-    public double f = 0.005;                // coulomb friction coeficient
-    public double b = 1;                    // viscous friction
-    public double m = 100;                  // mass of cart
+    public double f = 0.0005;               // coulomb friction coeficient
+    public double b_b = 0.005;              // viscous friction of beam
+    public double b_c = 0.005;              // viscous friction of cart
+    public double mass_b = 1;               // mass of ball
+    public double mass_c = 1;               // mass of cart
+    public double length = 0.01;            // length of beam
     public override void TakeStep(float dt)
     {
         t = integrator.RK4Step(integrator.x, t, dt, u);
@@ -22,16 +25,17 @@ public class PendulumModel : TimestepModel
     void Start()
     {
         integrator = new PendulumIntegrator();
-        integrator.setIC(0.5, 0, 0);
+        integrator.setIC(0, 0, 0, 0);
         ModelStart();
     }
 
     void Update()
     {
-        integrator.setParams(b, m, f, gForce);
+        integrator.setParams(mass_b, mass_c, b_b, b_c, length, f, gForce);
         u = Input.GetAxisRaw("Horizontal") * inputGain;
         Vector3 pos = cart.transform.position;
         pos.x = (float)integrator.x[0];
         cart.transform.position = pos;
+        // beam.transform.position = pos;
     }
 }
